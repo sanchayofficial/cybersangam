14bc860d8a56f3f8ba4ff63570fb8d85f4c68a85
Sanchay <sanchayofficial@gmail.com>
2026-02-11 19:12:19 +0530

    feb meetup announcement

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,4 +1,4 @@
-const targetDate = new Date("2026-01-31T12:00:00").getTime();
+const targetDate = new Date("2026-02-28T11:00:00").getTime();
 
     const countdown = setInterval(() => {
       const now = new Date().getTime();
